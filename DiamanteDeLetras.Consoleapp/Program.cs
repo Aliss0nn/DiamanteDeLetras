42dd93b6595f45b2dd440a0ba200f53d50f99b68
alisson <alisson@DESKTOP-DPK4TR6>
2023-03-20 21:16:28 -0300

    Add project files.

---
+++ DiamanteDeLetras.Consoleapp/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Runtime.ExceptionServices;
+namespace diamanteLetras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura = 0;
+
+            Console.Write("Qual a letra digitada: ");
+            string letraDigitada = Console.ReadLine();
+            letraDigitada = letraDigitada.ToUpper();
+
+            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+            alfabeto = alfabeto.ToUpper();
+
+            char[] letras = new char[26];
+            letras = alfabeto.ToCharArray();
+
+
+            for (int i = 0; i < letras.Length; i++)
+            {
+                if (letras[i] == letraDigitada.ToCharArray()[0])
+                {
+                    altura = i + 1;
+                    break;
+                }
+            }
+
+            int Espacoembranco = 0;
+            int limiteDentro = (altura * 2) - 1;
+            int espacoForaCima = 25;
+
+            for (int i = 0; i < altura; i++)
+            {
+                for (int j = 0; j < espacoForaCima; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                if (i == 0)
+                {
+                    Console.WriteLine(letras[i]);
+                    espacoForaCima--;
+                }
+
+                if (Espacoembranco == 0)
+                {
+                    Espacoembranco++;
+                }
+
+                else
+                {
+                    Console.Write(letras[i]);
+
+                    for (int k = 1; k <= Espacoembranco; k++)
+                    {
+                        Console.Write(" ");
+
+                    }
+
+                    if (limiteDentro > Espacoembranco) 
+                    {
+                        Espacoembranco += 2;
+                    }
+
+                    if (i < altura)
+                    {
+                        espacoForaCima--;
+
+                    }
+                    Console.Write(letras[i]);
+
+                    Console.WriteLine();
+
+                }
+
+
+            }
+
+
+            int espacoDentro2 = Espacoembranco - 2;
+            int limiteDentro2 = 0;
+            int espacoForaCima2 = espacoForaCima + 1;
+
+            for (int i = altura - 2; i >= 0; i--)
+            {
+                for (int j = espacoForaCima2; j >= 0; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                if (i == 0)
+                {
+                    Console.WriteLine(letras[i]);
+                    espacoForaCima2++;
+                }
+
+                if (espacoDentro2 == 1)
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.Write(letras[i]);
+
+                    for (int k = espacoDentro2 - 2; k > 0; k--)
+                    {
+                        Console.Write(" ");
+
+                    }
+
+                    if (limiteDentro2 < espacoDentro2)
+                    {
+                        espacoDentro2 -= 2;
+                    }
+
+                    if (i < altura)
+                    {
+                        espacoForaCima2++;
+
+                    }
+                    Console.Write(letras[i]);
+
+                    Console.WriteLine();
+
+                }
+
+
+            }
+
+        }
+    }
+}
+
